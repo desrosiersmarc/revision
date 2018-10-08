06e6551ca8643d28fc5aa995eb09fe800fcfcd59
marc desrosiers <mdesrosiers@norauto.fr>
2018-10-08 10:25:20 +0200

    save the file in a js file

--- week1/4-scrapping leboncoin js.js
+++ week1/4-scrapping leboncoin js.js
@@ -3,18 +3,28 @@ element=document.querySelectorAll('.CalendarDay');
 for (var i = 0; i < element.length ; i++){
   date_informations = element[i].attributes["aria-label"].value;
   date_to_display = date_informations.split(', ')[1].split(' as ')[0];
+
   price_to_display = element[i].firstElementChild.firstElementChild.firstElementChild.lastElementChild.innerHTML;
+  // stop on the last firstElementChild and test the number of Child Element.
+  // If one there is no day price
+  // else there is a day price and I can use the last child to find the price
   price_to_display = price_to_display.split(/\</)[1];
   price_to_display = price_to_display.split(/\>/)[1];
-  console.log(date_to_display + ": " + price_to_display + "€");
-  price_to_display = "";
+
+  available = element[i].classList;
+  available = available.contains('CalendarDay__blocked_calendar');
+
+  console.log(date_to_display + ": " + price_to_display + "€ " + available);
+  price_to_display = "0";
+  available = "no";
 }
 
+// CalendarDay__blocked_calendar class to identify
 
-
-#or for only one element
+// or for only one element
 element=document.querySelector('.CalendarDay')
 element.firstElementChild.firstElementChild.firstElementChild.lastElementChild.innerHTML
 
-#REGEX \>(.....) MATCH1
+// REGEX \>(.....) MATCH1
 
+// this class _1jnk2 dispay that the day is'nt available
