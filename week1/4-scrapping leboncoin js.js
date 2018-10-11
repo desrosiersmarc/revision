0bcbe97b833e1e2860ab725d9e9e0ffa6ffc9a5a
marc desrosiers <mdesrosiers@norauto.fr>
2018-10-11 09:06:45 +0200

    save first research of data extract

--- week1/4-scrapping leboncoin js.js
+++ week1/4-scrapping leboncoin js.js
@@ -4,12 +4,19 @@ for (var i = 0; i < element.length ; i++){
   date_informations = element[i].attributes["aria-label"].value;
   date_to_display = date_informations.split(', ')[1].split(' as ')[0];
 
-  price_to_display = element[i].firstElementChild.firstElementChild.firstElementChild.lastElementChild.innerHTML;
+  if (element[i].firstElementChild.firstElementChild.firstElementChild !== null) {
+    price_to_display = element[i].firstElementChild.firstElementChild.firstElementChild.lastElementChild.innerHTML;
+    console.log('1- ' + price_to_display);
+    price_to_display = price_to_display.split(/\</)[1];
+    console.log('2- ' + price_to_display);
+    price_to_display = price_to_display.split(/\>/)[1];
+    console.log('3- ' + price_to_display);
+  } else {
+    price_to_display = 0;
+  };
   // stop on the last firstElementChild and test the number of Child Element.
   // If one there is no day price
   // else there is a day price and I can use the last child to find the price
-  price_to_display = price_to_display.split(/\</)[1];
-  price_to_display = price_to_display.split(/\>/)[1];
 
   available = element[i].classList;
   available = available.contains('CalendarDay__blocked_calendar');
